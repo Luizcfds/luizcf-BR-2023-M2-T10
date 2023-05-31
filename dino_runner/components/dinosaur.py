2e64d53cb341808544f190292a29dde08b4a9385
Luizcfds <132014019+Luizcfds@users.noreply.github.com>
2023-05-30 22:52:54 -0300

    added obstacles and collision

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
@@ -6,7 +8,8 @@ Y_POS = 310
 Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
-class Dinosaur:
+
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -17,7 +20,6 @@ class Dinosaur:
         self.dino_duck = False
         self.dino_run = True
         self.dino_jump = False
-        
         
     def update(self, user_input):
         if self.dino_duck:
@@ -42,12 +44,9 @@ class Dinosaur:
             self.dino_duck = False
             self.dino_run = True
             self.dino_jump = False
-      
-      
             
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
-        
         
     def duck(self):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
@@ -55,10 +54,7 @@ class Dinosaur:
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
-        #self.dino_duck = True
-
-        
-        
+        self.dino_duck = False
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -66,8 +62,6 @@ class Dinosaur:
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
         self.step_index += 1
-
-
 
     def jump(self):
         self.image = JUMPING
